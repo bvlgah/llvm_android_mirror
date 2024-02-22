120515aab9bc3b4017e989ed0dcf64c095930fc1
Yi Kong <yikong@google.com>
2024-02-22 14:32:22 +0800

    kokoro: Use relative paths for OUT and DIST
    The build system doesn't like absolute paths.
    
    Test: N/A
    Change-Id: I9b5a973263c7561a2d2058c3c32164ff000b1065

--- kokoro/aosp-master-plus-llvm_build.sh
+++ kokoro/aosp-master-plus-llvm_build.sh
@@ -2,8 +2,6 @@
 set -e
 
 TOP=$(cd $(dirname $0)/../../.. && pwd)
-OUT=$TOP/out
-DIST=$TOP/dist
 
 # Kokoro will rsync back everything created by the build. Since we don't care
 # about any artifacts on this build, nuke EVERYTHING at the end of the build.
@@ -12,13 +10,14 @@ function cleanup {
 }
 trap cleanup EXIT
 
-# Fetch aosp-plus-llvm-master repo
-(cd $TOP; \
-  repo init -u https://android.googlesource.com/platform/manifest -b main --depth=1 < /dev/null; \
-  repo sync -c -j8)
+cd $TOP
 
-mkdir "${DIST}"
-DIST_DIR="${DIST}" OUT_DIR="${OUT}" $TOP/prebuilts/python/linux-x86/bin/python3 \
-  $TOP/toolchain/llvm_android/test_compiler.py --build-only --no-mlgo \
+# Fetch aosp-plus-llvm-master repo
+repo init -u https://android.googlesource.com/platform/manifest -b main --depth=1 < /dev/null
+repo sync -c -j8
+
+mkdir dist
+DIST_DIR=dist OUT_DIR=out prebuilts/python/linux-x86/bin/python3 \
+  toolchain/llvm_android/test_compiler.py --build-only --no-mlgo \
   --target aosp_cf_arm64_phone-staging-userdebug \
-  --clang-package-path ${KOKORO_GFILE_DIR} $TOP
+  --clang-package-path ${KOKORO_GFILE_DIR} .

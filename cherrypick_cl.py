f38f5fe334600441c1dd536223ae810d09c8b9cb
Yabin Cui <yabinc@google.com>
2024-07-08 11:45:59 -0700

    A few improvements in cherrypick_cl.py
    1. --verify-merge and --create-cl are probably always wanted. So enable
       them by default, and add options to disable them.
    2. Make --reason required. So we don't need to check it when creating a
       CL, which may fail with uncleaned patch files.
    3. Running cherrypick_cl.py with uncleaned patch files may create a new
       version of patch file. To avoid this, add PatchList.check_patches()
       to do basic checks in the patches.
    4. Verify merge using git am before using patch. Because it's easier
       to create a backport patch using llvm-project.tmp created by `git am`.
    5. Rename functions for different cherrypick modes.
    6. Use autopep8 to format code.
    
    Bug: none
    Test: run cherrypick_cl.py
    Change-Id: Ic465d55b9c99cb7c6a705b547019e4afcfd551e4

--- cherrypick_cl.py
+++ cherrypick_cl.py
@@ -42,17 +42,18 @@ def parse_args():
     parser = argparse.ArgumentParser(description="Cherry pick upstream LLVM patches.",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--sha', nargs='+', help='sha of patches to cherry pick')
+    parser.add_argument('--patch-file', help='Use custom patch file for --sha')
     parser.add_argument('--pr', help='Cherry pick from a GitHub PR, e.g., 84422')
     parser.add_argument(
         '--start-version', default='llvm',
         help="""svn revision to start applying patches. 'llvm' can also be used.""")
-    parser.add_argument('--verify-merge', action='store_true',
-                        help='check if patches can be applied cleanly')
-    parser.add_argument('--create-cl', action='store_true', help='create a CL')
     parser.add_argument('--bug', help='bug to reference in CLs created (if any)')
-    parser.add_argument('--reason', help='issue/reason to mention in CL subject line')
+    parser.add_argument('--reason', required=True,
+                        help='issue/reason to mention in CL subject line')
     parser.add_argument('--verbose', help='Enable logging')
-    parser.add_argument('--patch-file', help='Use custom patch file')
+    parser.add_argument('--no-verify-merge', action='store_true',
+                        help='check if patches can be applied cleanly')
+    parser.add_argument('--no-create-cl', action='store_true', help='create a CL')
     args = parser.parse_args()
     return args
 
@@ -68,13 +69,13 @@ def parse_start_version(start_version: str) -> int:
 @dataclass
 class PatchItem:
     metadata: Dict[str, Any]
-        # info: Optional[List[str]]
-        # title: str
+    # metadata['info']: Optional[List[str]]
+    # metadata['title']: str
     platforms: List[str]
     rel_patch_path: str
     version_range: Dict[str, Optional[int]]
-        # from: Optional[int]
-        # until: Optional[int]
+    # version_range['from']: Optional[int]
+    # version_range['until']: Optional[int]
 
     @classmethod
     def from_dict(cls, d: Dict[str, Any]) -> PatchItem:
@@ -86,6 +87,10 @@ class PatchItem:
 
     def to_dict(self) -> Dict[str, Any]:
         return dataclasses.asdict(self, dict_factory=collections.OrderedDict)
+
+    @property
+    def title(self) -> str:
+        return self.metadata['title']
 
     @property
     def is_local_patch(self) -> bool:
@@ -100,7 +105,7 @@ class PatchItem:
     @property
     def pr_link(self) -> str:
         m = next(re.match(r'Pull Request: (.+)', line)
-            for line in open(f'patches/{self.rel_patch_path}'))
+                 for line in open(f'patches/{self.rel_patch_path}'))
 
         assert m, f'No PR link found in: {self.rel_patch_path}'
         return m.group(1)
@@ -151,7 +156,29 @@ class PatchList(list):
             json.dump(array, fh, indent=4, separators=(',', ': '), sort_keys=True)
             fh.write('\n')
 
-def generate_patch_files(sha_list: List[str], start_version: int, patch_list: PatchList) -> PatchList:
+    def check_patches(self) -> bool:
+        patch_files = set()
+        for patch in self:
+            if not patch.start_version:
+                logging.error(f"'{patch.title}' doesn't have a start version")
+                return False
+            if patch.end_version and patch.end_version <= patch.start_version:
+                logging.error(
+                    f"'{patch.title}' has end version <= start version. It should be removed.")
+                return False
+            patch_file = paths.SCRIPTS_DIR / 'patches' / patch.rel_patch_path
+            if not patch_file.is_file():
+                logging.error(f"{patch_file} used by '{patch.title}' isn't a file")
+                return False
+            if patch_file in patch_files:
+                logging.error(f"{patch_file} is used by multiple patches")
+                return False
+            patch_files.add(patch_file)
+        return True
+
+
+def create_patches_for_sha_list(sha_list: List[str], start_version: int, patch_list: PatchList
+                                ) -> PatchList:
     """ generate upstream cherry-pick patch files """
     upstream_dir = paths.TOOLCHAIN_LLVM_PATH
     fetch_upstream()
@@ -172,7 +199,7 @@ def generate_patch_files(sha_list: List[str], start_version: int, patch_list: Pa
         info: Optional[List[str]] = []
         title = '[UPSTREAM] ' + commit_subject.strip()
         end_version = sha_to_revision(sha)
-        metadata = { 'info': info, 'title': title }
+        metadata = {'info': info, 'title': title}
         platforms = ['android']
         version_range: Dict[str, Optional[int]] = {
             'from': start_version,
@@ -181,7 +208,9 @@ def generate_patch_files(sha_list: List[str], start_version: int, patch_list: Pa
         result.append(PatchItem(metadata, platforms, rel_patch_path, version_range))
     return result
 
-def add_new_patch_for_sha(patch_file: Path, sha: str, start_version: int, patch_list: PatchList) -> PatchList:
+
+def create_patch_for_sha_with_patch_file(patch_file: Path, sha: str, start_version: int,
+                                         patch_list: PatchList) -> PatchList:
     """ add patch files for sha"""
     upstream_dir = paths.TOOLCHAIN_LLVM_PATH
     result = []
@@ -192,14 +221,14 @@ def add_new_patch_for_sha(patch_file: Path, sha: str, start_version: int, patch_
     file_path = paths.SCRIPTS_DIR / 'patches' / rel_patch_path
     with open(patch_file, 'r') as source, open(file_path, 'w') as dest:
         for line in source:
-           dest.write(line)
+            dest.write(line)
 
     commit_subject = check_output(
         f'git log -n1 --format=%s {sha}', shell=True, cwd=upstream_dir)
     info: Optional[List[str]] = []
     title = '[UPSTREAM] ' + commit_subject.strip()
     end_version = sha_to_revision(sha)
-    metadata = { 'info': info, 'title': title }
+    metadata = {'info': info, 'title': title}
     platforms = ['android']
     version_range: Dict[str, Optional[int]] = {
         'from': start_version,
@@ -207,6 +236,7 @@ def add_new_patch_for_sha(patch_file: Path, sha: str, start_version: int, patch_
     }
     result.append(PatchItem(metadata, platforms, rel_patch_path, version_range))
     return result
+
 
 def get_full_sha(upstream_dir: Path, short_sha: str) -> str:
     return check_output(['git', 'rev-parse', short_sha], cwd=upstream_dir).strip()
@@ -229,17 +259,17 @@ def create_cl(new_patches: PatchList, reason: str, bug: Optional[str], cherry: b
         if element.startswith('--reason'):
             del argv_deepcopy[i]
             if element == '--reason':
-              del argv_deepcopy[i]
+                del argv_deepcopy[i]
             break
 
     for patch in new_patches:
-        if cherry: # Add SHA and title for each cherry-pick.
+        if cherry:  # Add SHA and title for each cherry-pick.
             sha = patch.sha[:11]
             subject = patch.metadata['title']
             if subject.startswith('[UPSTREAM] '):
                 subject = subject[len('[UPSTREAM] '):]
             commit_line = sha + ' ' + subject
-        else: # Add link to differential revision.
+        else:  # Add link to differential revision.
             commit_line = patch.pr_link
         commit_lines.append(commit_line)
     commit_lines.append('')
@@ -256,24 +286,25 @@ def create_cl(new_patches: PatchList, reason: str, bug: Optional[str], cherry: b
     commit_lines += ['', 'Test: N/A']
     check_call(['git', 'commit', '-m', '\n'.join(commit_lines)])
 
-def create_patch(pr, start_version) -> PatchList:
-    pr_url=f'https://api.github.com/repos/llvm/llvm-project/pulls/{pr}'
+
+def create_patch_for_pr(pr: str, start_version: int) -> PatchList:
+    pr_url = f'https://api.github.com/repos/llvm/llvm-project/pulls/{pr}'
     patch_url_req = urllib.request.Request(f'https://github.com/llvm/llvm-project/pull/{pr}.diff',
-                                        method="HEAD")
+                                           method="HEAD")
     patch_url = urllib.request.urlopen(patch_url_req).url
 
     # TODO: Add commit body and author details as well.
     with urllib.request.urlopen(pr_url) as response:
-      data = json.load(response)
-    title=data['title']
+        data = json.load(response)
+    title = data['title']
     assert title, f'Title not found for {pr}'
     print(f'Creating a patch for {title}')
-    file_name=f'{pr}.patch'
-    abs_file_name= paths.SCRIPTS_DIR / 'patches' / file_name
+    file_name = f'{pr}.patch'
+    abs_file_name = paths.SCRIPTS_DIR / 'patches' / file_name
     # Download the file from `patch_url` and save in `abs_file_name`:
     urllib.request.urlretrieve(patch_url, abs_file_name)
     # Add link to Differential Revision at the beginning of the file
-    patch_prefix=f'Pull Request: {patch_url}\nSubject: {title}'
+    patch_prefix = f'Pull Request: {patch_url}\nSubject: {title}'
     with open(abs_file_name, 'r+') as f:
         content = f.read()
         f.seek(0, 0)
@@ -284,14 +315,15 @@ def create_patch(pr, start_version) -> PatchList:
     info: Optional[List[str]] = []
     rel_patch_path = f'{file_name}'
     end_version = None
-    metadata = { 'info': info, 'title': title }
+    metadata = {'info': info, 'title': title}
     platforms = ['android']
     version_range: Dict[str, Optional[int]] = {
-       'from': start_version,
-       'until': end_version,
+        'from': start_version,
+        'until': end_version,
     }
     result.append(PatchItem(metadata, platforms, rel_patch_path, version_range))
     return result
+
 
 def find_version(sha, patch_list, start_version) -> int:
     """ Return the next version for the given SHA and update end_revision if needed"""
@@ -303,20 +335,21 @@ def find_version(sha, patch_list, start_version) -> int:
     # Find the latest version
     for i, item in enumerate(patch_list):
         if item.rel_patch_path.startswith(target):
-           last_idx = i
-           name = item.rel_patch_path.removesuffix('.patch')
+            last_idx = i
+            name = item.rel_patch_path.removesuffix('.patch')
 
     # If this patch is not new, update the end_revision for Vn
     if last_idx != -1:
-       patch_list[last_idx].version_range['until'] = start_version
-       if name == target:
-        return 2
-       prefix = target + f'-v'
-       version = int(name.removeprefix(prefix)) + 1
+        patch_list[last_idx].version_range['until'] = start_version
+        if name == target:
+            return 2
+        prefix = target + f'-v'
+        version = int(name.removeprefix(prefix)) + 1
 
     return version
 
-def main():
+
+def main() -> bool:
     args = parse_args()
     level = logging.DEBUG if args.verbose else logging.INFO
     logging.basicConfig(level=level)
@@ -327,33 +360,35 @@ def main():
     )
     if args.pr:
         start_version = parse_start_version(args.start_version)
-        new_patches = create_patch(args.pr, start_version)
+        new_patches = create_patch_for_pr(args.pr, start_version)
         patch_list.extend(new_patches)
     elif args.sha:
         start_version = parse_start_version(args.start_version)
         if args.patch_file:
-            assert len(args.sha) == 1,  f'error: --patch-file only requires 1 sha, but the size of sha list is {len(args.sha)}'
-            new_patches = add_new_patch_for_sha(args.patch_file, args.sha[0], start_version, patch_list)
+            assert len(
+                args.sha) == 1,  f'error: --patch-file only requires 1 sha, but the size of sha list is {len(args.sha)}'
+            new_patches = create_patch_for_sha_with_patch_file(
+                args.patch_file, args.sha[0], start_version, patch_list)
         else:
-            new_patches = generate_patch_files(args.sha, start_version, patch_list)
+            new_patches = create_patches_for_sha_list(args.sha, start_version, patch_list)
         patch_list.extend(new_patches)
 
     patch_list.sort()
     patch_list.save_to_file()
+    if not patch_list.check_patches():
+        return False
 
-    if args.verify_merge:
+    if not args.no_verify_merge:
         print('Verifying merge...')
-        print('Verifying merge with patch ...')
-        source_manager.setup_sources()
         print('Verifying merge with git am ...')
         source_manager.setup_sources(git_am=True)
-    if args.create_cl:
-        if not args.reason:
-            print('error: --create-cl requires --reason')
-            exit(1)
+        print('Verifying merge with patch ...')
+        source_manager.setup_sources()
+    if not args.no_create_cl:
         cherry = True if args.sha else False
         create_cl(new_patches, args.reason, args.bug, cherry)
+    return True
 
 
 if __name__ == '__main__':
-    main()
+    sys.exit(0 if main() else 1)

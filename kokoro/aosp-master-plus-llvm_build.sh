507f8433ca194a27272731f487870d9bad64d2a5
Yi Kong <yikong@google.com>
2024-07-18 16:58:56 +0900

    kokoro: actually print out failed build commands
    Since test_compiler.py exits with failure, the print command doesn't
    actually got executed. Moved it to the cleanup function.
    
    Test: manual
    Change-Id: I09631b15f5e7bb1f29c4fb13b25d5364296e580f

--- kokoro/aosp-master-plus-llvm_build.sh
+++ kokoro/aosp-master-plus-llvm_build.sh
@@ -3,9 +3,13 @@ set -e
 
 TOP=$(cd $(dirname $0)/../../.. && pwd)
 
-# Kokoro will rsync back everything created by the build. Since we don't care
-# about any artifacts on this build, nuke EVERYTHING at the end of the build.
 function cleanup {
+  if [ -f out/dist/logs/error.log ]; then
+    cat out/dist/logs/error.log
+  fi
+
+  # Kokoro will rsync back everything created by the build. Since we don't care
+  # about any artifacts on this build, nuke EVERYTHING at the end of the build.
   rm -rf "${TOP}"/*
 }
 trap cleanup EXIT
@@ -36,8 +40,4 @@ prebuilts/python/linux-x86/bin/python3 \
   toolchain/llvm_android/test_compiler.py --build-only \
   --target ${AOSP_BUILD_TARGET}-trunk_staging-userdebug \
   --clang-package-path ${KOKORO_GFILE_DIR} .
-
-if [ -f out/dist/logs/error.log ]; then
-  cat out/dist/logs/error.log
-fi
 
